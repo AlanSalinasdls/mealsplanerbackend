8adeb7e95506ab833d33fb0cbe12a3ee8f914dad
AlanSalinasdls <alansdlsdev@gmail.com>
2024-12-18 14:08:05 -0600

    chore: fix spelling mistake

--- mealsplannerbackend/wsgi.py
+++ mealsplannerbackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mealsplanerbackend project.
+WSGI config for mealsplannerbackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealsplanerbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealsplannerbackend.settings')
 
 application = get_wsgi_application()
